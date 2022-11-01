3d6b1c9c2770ed18268a85d084c1c5b352235908
AhmedHumais <m.ahmedhumais@gmail.com>
2022-11-01 18:11:47 +0400

    added tf2 buffer header

--- include/HEAR_ROS2/Custom/ROSUnit_SLAM.hpp
+++ include/HEAR_ROS2/Custom/ROSUnit_SLAM.hpp
@@ -7,6 +7,7 @@
 #include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
 #include "HEAR_core/Block.hpp"
